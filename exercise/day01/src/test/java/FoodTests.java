0cba248884e1ebe89d4bd117ed449015285c35b6
Petikoch <petikoch@gmail.com>
2023-12-01 17:02:34 +0100

    r - removed unused imports

--- exercise/day01/src/test/java/FoodTests.java
+++ exercise/day01/src/test/java/FoodTests.java
@@ -1,12 +1,8 @@
 import food.Food;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 import static java.time.LocalDate.of;
 import static java.util.UUID.randomUUID;
