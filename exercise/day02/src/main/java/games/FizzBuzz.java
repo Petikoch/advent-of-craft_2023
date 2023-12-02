88d533ad3a16df03ebbdad1d1af27cfd14c71a9f
Petikoch <petikoch@gmail.com>
2023-12-02 15:31:33 +0100

    R - simplified code (only one level of indentation)

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -1,27 +1,38 @@
 package games;
 
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
     private FizzBuzz() {
     }
-    
+
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+        if (input <= 0) {
             throw new OutOfRangeException();
         }
+        if (input > 100) {
+            throw new OutOfRangeException();
+        }
+
+        if (isFizz(input) && isBuzz(input)) {
+            return FIZZ + BUZZ;
+        }
+        if (isFizz(input)) {
+            return FIZZ;
+        }
+        if (isBuzz(input)) {
+            return BUZZ;
+        }
+        return input.toString();
+    }
+
+    private static boolean isFizz(Integer input) {
+        return input % 3 == 0;
+    }
+
+    private static boolean isBuzz(Integer input) {
+        return input % 5 == 0;
     }
 }
