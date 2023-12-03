b7d00982c6f3927438466a3ca216ca4c0a6d5636
Petikoch <petikoch@gmail.com>
2023-12-03 15:13:37 +0100

    R - simplify test and bring into arrange/act/assert structure

--- exercise/day03/src/test/java/PopulationTests.java
+++ exercise/day03/src/test/java/PopulationTests.java
@@ -9,33 +9,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PopulationTests {
 
-    private static final Population POPULATION = new Population(
-            List.of(
-                    new Person("Peter", "Griffin")
-                            .addPet(PetType.CAT, "Tabby", 2),
-                    new Person("Stewie", "Griffin")
-                            .addPet(PetType.CAT, "Dolly", 3)
-                            .addPet(PetType.DOG, "Brian", 9),
-                    new Person("Joe", "Swanson")
-                            .addPet(PetType.DOG, "Spike", 4),
-                    new Person("Lois", "Griffin")
-                            .addPet(PetType.SNAKE, "Serpy", 1),
-                    new Person("Meg", "Griffin")
-                            .addPet(PetType.BIRD, "Tweety", 1),
-                    new Person("Chris", "Griffin")
-                            .addPet(PetType.TURTLE, "Speedy", 4),
-                    new Person("Cleveland", "Brown")
-                            .addPet(PetType.HAMSTER, "Fuzzy", 1)
-                            .addPet(PetType.HAMSTER, "Wuzzy", 2),
-                    new Person("Glenn", "Quagmire")
-            )
-    );
-
     @Test
     void whoOwnsTheYoungestPet() {
-        var filtered = POPULATION.personWithYoungestPet();
+        Person lois = new Person("Lois", "Griffin")
+                .addPet(PetType.SNAKE, "Serpy", 1);
+        Population testPopulation = new Population(
+                List.of(
+                        new Person("Peter", "Griffin")
+                                .addPet(PetType.CAT, "Tabby", 2),
+                        new Person("Stewie", "Griffin")
+                                .addPet(PetType.CAT, "Dolly", 3)
+                                .addPet(PetType.DOG, "Brian", 9),
+                        new Person("Joe", "Swanson")
+                                .addPet(PetType.DOG, "Spike", 4),
+                        lois,
+                        new Person("Meg", "Griffin")
+                                .addPet(PetType.BIRD, "Tweety", 1),
+                        new Person("Chris", "Griffin")
+                                .addPet(PetType.TURTLE, "Speedy", 4),
+                        new Person("Cleveland", "Brown")
+                                .addPet(PetType.HAMSTER, "Fuzzy", 1)
+                                .addPet(PetType.HAMSTER, "Wuzzy", 2),
+                        new Person("Glenn", "Quagmire")
+                )
+        );
 
-        assert filtered != null;
-        assertThat(filtered.firstName()).isEqualTo("Lois");
+        var actual = testPopulation.personWithYoungestPet();
+
+        assert actual != null;
+        assertThat(actual).isEqualTo(lois);
     }
 }
