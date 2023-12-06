ad9395be418d16ba2ae815b55dce7bbfbe3b4765
Petikoch <petikoch@gmail.com>
2023-12-06 15:34:56 +0100

    R - add typesafe paramterized tests

--- exercise/day06/src/test/java/games/FizzBuzzTests.java
+++ exercise/day06/src/test/java/games/FizzBuzzTests.java
@@ -1,10 +1,15 @@
 package games;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class FizzBuzzTests {
 
@@ -31,18 +36,61 @@ class FizzBuzzTests {
             "30, FizzBuzz",
             // ...
             "90, FizzBuzz",
+            // ...
             "98, 98",
             "99, Fizz",
             "100, Buzz",
             "101, OutOfRangeException",
     })
-    void converts_according_specs(int input, String expectedOutput) throws OutOfRangeException {
+    void stringbased_specs(int input, String expectedOutput) throws OutOfRangeException {
         if (OutOfRangeException.class.getSimpleName().equals(expectedOutput)) {
             assertThatThrownBy(() -> FizzBuzz.convert(input))
                     .isInstanceOf(OutOfRangeException.class);
         } else {
             assertThat(FizzBuzz.convert(input)).isEqualTo(expectedOutput);
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("testDataProvider")
+    void typesafe_specs(int input, String expectedOutput, Class<?> expectedExceptionClass) throws OutOfRangeException {
+        if (expectedExceptionClass != null) {
+            assertThatThrownBy(() -> FizzBuzz.convert(input))
+                    .isInstanceOf(expectedExceptionClass);
+        } else {
+            assertThat(FizzBuzz.convert(input)).isEqualTo(expectedOutput);
+        }
+    }
+
+    static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                arguments(-1, null, OutOfRangeException.class),
+                arguments(0, null, OutOfRangeException.class),
+                arguments(1, "1", null),
+                arguments(2, "2", null),
+                arguments(3, "Fizz", null),
+                arguments(4, "4", null),
+                arguments(5, "Buzz", null),
+                arguments(6, "Fizz", null),
+                arguments(7, "7", null),
+                // ...
+                arguments(9, "Fizz", null),
+                arguments(10, "Buzz", null),
+                arguments(11, "11", null),
+                // ...
+                arguments(15, "FizzBuzz", null),
+                // ...
+                arguments(20, "Buzz", null),
+                // ...
+                arguments(30, "FizzBuzz", null),
+                // ...
+                arguments(90, "FizzBuzz", null),
+                // ...
+                arguments(98, "98", null),
+                arguments(99, "Fizz", null),
+                arguments(100, "Buzz", null),
+                arguments(101, null, OutOfRangeException.class)
+        );
     }
 
 }
